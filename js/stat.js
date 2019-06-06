922fbf5f983828f8f92a8925f87fbee899e863c0
ely <ely137137@gmail.com>
2019-06-06 14:46:37 +0300

    Сделана функция отрисовки столбика, значения вынесены в переменные

--- js/stat.js
+++ js/stat.js
@@ -2,7 +2,14 @@
 
 var cloudWidth = 420;
 var cloudHeight = 270;
-var randomSaturation = Math.floor(Math.random() * 100);
+var columnWidth = 40;
+var columnHeight = 150;
+var columnY = 250;
+var startTextX = 140;
+var startTextY = 260;
+var textY = 230;
+var gap = 50;
+var spaceBetween = columnWidth + gap;
 
 var makeCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
@@ -25,6 +32,24 @@ var getMaxElement = function (arr) {
   return maxElement;
 };
 
+var getColumn = function (ctx, i, namesArray, timesArray, maxTime) {
+  var columnGap = spaceBetween * i;
+  ctx.fillStyle = '#000000';
+  ctx.globalAlpha = 1;
+  makeText(ctx, startTextX + columnGap, startTextY, namesArray[i]);
+
+  namesArray[i] === 'Вы' ? ctx.fillStyle = 'rgba(255, 0, 0, 1)'
+                         :
+                         ctx.fillStyle = 'rgba' + '(' + '31' + ', ' +  '58' + ', ' + '147' + ', ' + Math.random() + ')';
+
+  var height = (timesArray[i] * columnHeight) / maxTime;
+  ctx.fillRect(startTextX + columnGap, columnY - height, columnWidth, height);
+  ctx.fillStyle = '#000000';
+  ctx.globalAlpha = 1;
+  makeText(ctx, startTextX + columnGap, textY - height, Math.floor(timesArray[i]));
+}
+
+
 window.renderStatistics = function (ctx, names, times) {
   makeCloud(ctx, 110, 20, 'rgba(0, 0, 0, 0.7)');
   makeCloud(ctx, 100, 10, '#ffffff');
@@ -38,19 +63,6 @@ window.renderStatistics = function (ctx, names, times) {
   var maxTime = getMaxElement(times);
 
   for (var i = 0; i < names.length; i++) {
-    var columnGap = 90 * i;
-    ctx.fillStyle = '#000000';
-    ctx.globalAlpha = 1;
-    makeText(ctx, 140 + columnGap, 260, names[i]);
-
-    names[i] === 'Вы' ? ctx.fillStyle = 'rgba(255, 0, 0, 1)'
-                      :
-                      ctx.fillStyle = 'rgba' + '(' + '31' + ', ' +  '58' + ', ' + '147' + ', ' + Math.random() + ')';
-
-    var height = (times[i] * 150) / maxTime;
-    ctx.fillRect(140 + columnGap, 250 - height, 40, 0 + height);
-    ctx.fillStyle = '#000000';
-    ctx.globalAlpha = 1;
-    makeText(ctx, 140 + columnGap, 230 - height, Math.floor(times[i]));
+    getColumn(ctx, i, names, times, maxTime);
   }
 };
